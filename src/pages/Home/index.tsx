4fbd822524e986cef736cbf515f7901d7f4bcc66
WynnieYang <hh301489@gmail.com>
2023-05-23 07:41:22 +0800

    wynnie_update_code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,7 +23,7 @@ const Home: React.FC<any> = () => {
                     <Nav/>
                     <div className='content w-100 flex flex-row display_start_center'>
                         <div className={`${trianglevanish ? '' : 'triangle'}`} style={{position:'absolute',left:'45%',top:'40%'}}></div>
-                        <div className=''><img className={`w-100 ${imgShow ? 'img-opacity' : 'opacity-0'}`} src='https://uc5c5733d49bbab186e9a445d4e5.previews.dropboxusercontent.com/p/thumb/AB7HD-9HjTdAtQY1y4IOLmogbmdlF5X86Brkt1uiNg9qCD4rNBuR4eRJDzsigXztcFljDg-8B03ggAnygkc4uCXkkgQPF-cOI6SI_DB_VueCbFVaXRVmAeCFVfNyaouuUKJ9HZHt-DK00ilrEzGrWlUlW5XDqQdwjX8fMni36kqYr2gN4PUoIg0PbhWiP2uZMBh2vY-uTF9p4lb2MZM-GDAACiHcwxL3OjdhBNSy-5MGGAAZdzGkoynAZDe_Pn1LQpQJitkBCYWfZPvLbcwJPZQz3Q9Pdk-l3JrZYXhYb5lHFi2jX52hE9tVJ6Jb9U1uXSqunnCBrsIXxxQ8oixgE1szx6VNoUOzXvHxnSWR76GCKZU-nIwkEsdjrUNlcH5fQC9wG4sKvc84wsT9NyEvxpiKIkqFKBqitmKe7JfDvAAx0xh3YNQXD_HBgq31-A2U9cclo26NXPT21NSTi0xJMokN/p.jpeg'></img></div>
+                        <div className=''><img className={`w-100 ${imgShow ? 'img-opacity' : 'opacity-0'}`} src={require("./images/IMG_0515.jpg")}></img></div>
                     </div>
                 </div>
                 
