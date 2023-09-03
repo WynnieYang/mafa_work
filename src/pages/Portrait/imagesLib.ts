1b540a6be86a150b86d9ebbe97db3d4dc6e6d14a
WynnieYang <hh301489@gmail.com>
2023-09-03 20:44:44 +0800

    wynnie_change_index

--- src/pages/Portrait/imagesLib.ts
+++ src/pages/Portrait/imagesLib.ts
@@ -3,42 +3,46 @@ import React from "react";
 export const portraitImages = {
     blockOne: [
         {
-            url:'./images/IMG_5664.jpg'
+            url:'./images/IMG_7562-1.jpg'
         },
         {
-            url:'./images/IMG_5716.jpg'
+            url:'./images/IMG_9519.jpg'
         },
-        {
-            url:'./images/IMG_5896.jpg'
-        },
-        {
-            url:'./images/IMG_6249.jpg'
-        },
-    ],
-    blockTwo: [
         {
             url:'./images/IMG_6546.jpg'
         },
         {
             url:'./images/IMG_6682.jpg'
         },
-        {
-            url:'./images/IMG_7562-1.jpg'
-        },
-        {
-            url:'./images/IMG_9519.jpg'
-        },
+        
     ],
-    blockThree: [
-        {
-            url:'./images/IMG_6899.jpg'
-        },
+    blockTwo: [
         {
             url:'./images/IMG_9011.jpg'
         },
         {
             url:'./images/IMG_9418.jpg'
         },
+        
+        {
+            url:'./images/IMG_5896.jpg'
+        },
+        {
+            url:'./images/IMG_6249.jpg'
+        },
+        
+    ],
+    blockThree: [
+        {
+            url:'./images/IMG_5664.jpg'
+        },
+        {
+            url:'./images/IMG_5716.jpg'
+        },
+        {
+            url:'./images/IMG_6899.jpg'
+        },
+        
         {
             url:'./images/nkwoIMG_9184.jpg'
         },
